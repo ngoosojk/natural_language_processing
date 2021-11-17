eb7c22f8c831c22e7d935e58e8cef09a9c379592
Joseph <josephkagotho04@gmail.com>
2021-11-17 11:43:26 +0300

    commit message

---
+++ Colab Notebooks/natural_language_processing/twitter_credentials.py
@@ -0,0 +1,8 @@
+
+# Variables which contain the user credentials needed to access the Twitter API
+# Copy and paste your credentials within the quotation marks '' as these are string variables
+
+api_key = '2zuuYz4lFNsviibCSvykfQrCD'
+api_secret_key =  'pZaA78LCKytyZDWJrMk9FxO09GIK9OkryyRmfu4iiMRmgTMqh9'
+access_token = '898065281884598273-BALTqXATLNxAJZ0M8z7E0f1uh4lvIpb'
+access_token_secret = 'mp4uaQmpX5pv0j8djWlFxpYtvxcZr1mFhZogDgy9vKOvz'
